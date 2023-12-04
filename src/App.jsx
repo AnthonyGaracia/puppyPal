660430c6d6653c4dc3d0473a73a540d76f9e4e6e
Tony Garcia <tonygarcia@Tonys-Air.attlocal.net>
2023-12-04 14:40:36 -0800

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
-
+import {puppyList} from './data.js'
+console.log(puppies)  
 import './App.css'
 
+function App() {
 
-    
+const [puppies, setPuppies] = useState(puppyList);
+
       <div>
+        {
+          puppies.map((puppy) => {
+            return <p>{puppy.name}</p>
+          })
+        }
 
       </div>
     
-    
-
+ return  
+} 
 
 export default App
+
+
